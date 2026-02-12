14b7f17ba905064331d010ad00430b2da67c2f20
María Bazán <formacionmbm@gmail.com>
2026-02-12 20:17:45 +0100

    solucionado

--- src/main/java/com/practica/ejercicio2/services/ServicioBusquedas.java
+++ src/main/java/com/practica/ejercicio2/services/ServicioBusquedas.java
@@ -63,6 +63,8 @@ public class ServicioBusquedas implements Busquedas {
         log.debug("[importeMinimo:{}][importeMaximo:{}]", importeMinimo, importeMaximo);
 
         try {
+            if(importeMaximo<0 || importeMinimo<0)
+                return List.of();
             return repositorio.findByEntreImportes(importeMinimo, importeMaximo);
         } catch (Exception e) {
             log.error("General Error", e);
